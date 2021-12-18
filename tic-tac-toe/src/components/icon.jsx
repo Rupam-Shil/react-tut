a71f5dcc16b4983b8f4496ec8a6b5a599d42800e
Rupam Shil <rupamshil111@gmail.com>
2021-12-18 19:50:14 +0530

    tic tac toe react added

---
+++ tic-tac-toe/src/components/icon.jsx
@@ -0,0 +1,13 @@
+import { FaTimes, FaPen, FaRegCircle, FaCircle } from 'react-icons/fa';
+const Icon = ({ name }) => {
+	switch (name) {
+		case 'circle':
+			return <FaCircle className="icons" />;
+		case 'cross':
+			return <FaTimes className="icons" />;
+		default:
+			return <FaPen className="icons" />;
+	}
+};
+
+export default Icon;
