b012bdd93edb1d1ac0fb3a87e08c685ed77e59d5
Rupam Shil <rupamshil111@gmail.com>
2021-12-19 19:59:48 +0530

    context api added

---
+++ side-prac/side-one-context/src/problem.jsx
@@ -0,0 +1,26 @@
+// import React, { useState } from 'react';
+
+// const GrandChild = ({ brandname }) => {
+// 	return (
+// 		<div>
+// 			<Child brand={brandname} />
+// 			<h3>Grandchild:</h3>
+// 		</div>
+// 	);
+// };
+
+// const Child = ({ brand }) => {
+// 	return <div>{brand}</div>;
+// };
+
+// const App = () => {
+// 	const [brandname, setBrandname] = useState('amazon');
+// 	return (
+// 		<div>
+// 			Hello
+// 			<GrandChild brandname={brandname} />
+// 		</div>
+// 	);
+// };
+
+// export default App;
