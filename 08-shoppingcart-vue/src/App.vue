03944aca6215a24ba9ad566a80aafb05dae6a2d8
Rupam Shil <rupamshil111@gmail.com>
2021-12-31 22:43:28 +0530

    vue version started

---
+++ 08-shoppingcart-vue/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="home">
+		<MobileView color="#EBEAEF"></MobileView>
+		<MobileView color="#F2F0F1"></MobileView>
+	</div>
+</template>
+<script setup>
+import MobileView from './components/MobileView.vue';
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900&display=swap');
+*,
+*::before,
+*::after {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+body {
+	font-family: 'Poppins', sans-serif;
+}
+:root {
+	--bg: #e5e5e5;
+}
+.home {
+	width: 100vw;
+	min-height: 100vh;
+	background: var(--bg);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 4rem;
+}
+@media only screen and (max-width: 768px) {
+	.home {
+		padding: 2rem;
+		flex-direction: column;
+		gap: 2rem;
+	}
+}
+</style>
---
+++ 08-shoppingcart-vue/src/components/MobileView.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="box">
+		<slot></slot>
+	</div>
+</template>
+
+<script setup>
+import { defineProps, toRefs } from 'vue';
+
+const props = defineProps({
+	color: {
+		type: String,
+		default: 'red',
+	},
+});
+
+const { color } = toRefs(props);
+</script>
+
+<style lang="scss" scoped>
+.box {
+	width: 350px;
+	height: 650px;
+	background: v-bind(color);
+	border-radius: 2rem;
+	flex-shrink: 0;
+	box-shadow: 0 0 10px hsl(0, 6%, 86%), 0 0 20px hsl(0, 0%, 88%),
+		0 0 25px hsl(0, 0%, 93%);
+	overflow-y: scroll;
+	&::-webkit-scrollbar {
+		display: none;
+	}
+}
+</style>
