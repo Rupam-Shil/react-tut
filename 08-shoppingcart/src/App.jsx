ed6b4823ae0c33ff376f47dabcdf24a3153e5c3e
Rupam Shil <rupamshil111@gmail.com>
2021-12-31 19:26:37 +0530

    shopping card added

--- 08-shoppingcart/src/App.jsx
+++ 08-shoppingcart/src/App.jsx
@@ -3,7 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
+
+import { Container, Row, Col } from 'reactstrap';
+
 import BuyPage from './components/BuyPage';
+import Cart from './components/Cart';
 
 function App() {
 	const [cartItem, setCartItem] = useState([]);
@@ -12,6 +16,7 @@ function App() {
 		const isAlreadyAdded = cartItem.findIndex((array) => array.id === item.id);
 		if (isAlreadyAdded !== -1) {
 			toast('already added in cart', { type: 'error' });
+			return;
 		}
 
 		setCartItem([...cartItem, item]);
@@ -28,7 +33,17 @@ function App() {
 
 	return (
 		<div className="App">
-			<BuyPage addInCart={addInCart} />
+			<Container fluid>
+				<ToastContainer />
+				<Row>
+					<Col md="8">
+						<BuyPage addInCart={addInCart}></BuyPage>
+					</Col>
+					<Col md="4">
+						<Cart cartItem={cartItem} removeItem={removeItem} buyNow={buyNow} />
+					</Col>
+				</Row>
+			</Container>
 		</div>
 	);
 }
--- 08-shoppingcart/src/components/BuyPage.jsx
+++ 08-shoppingcart/src/components/BuyPage.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import faker from 'faker';
 import { Container, Col, Row } from 'reactstrap';
 import { APIKEY } from '../utils/SECRET';
+import CartItem from './CartItem';
 
 const url = 'https://api.pexels.com/v1/search?query=laptop&per_page=6&page=1';
 
@@ -35,8 +36,8 @@ const BuyPage = ({ addInCart }) => {
 			<h1 className="text-success text-center">Buy Page</h1>
 			<Row>
 				{product.map((each) => (
-					<Col key={each.id}>
-						<span>{each.productName}</span>
+					<Col md={4} key={each.id}>
+						<CartItem addInCart={addInCart} product={each} />
 					</Col>
 				))}
 			</Row>
